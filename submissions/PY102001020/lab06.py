4156d7d0c4f3d6b4d45ef734e9ffac9b0e3afba8
Nwe Ni Htoo <htoo.nweni@gmail.com>
2026-04-09 19:06:37 +0630

    Lab05 Submission - PY102001020 (#263)
    * Sbumit Lab00 - PY102001020
    
    * Sumbit Lab 01 - PY102001020
    
    * Submit Lab01 - PY102001020
    
    * Submit Lab01 - PY102001020
    
    * Submit Lab01 - PY102001020
    
    * Submit lab01 - PY102001020
    
    * Delete submissions/PY102001020/lab01.py
    
    * Lab01 Submission - PY102001020
    
    * Submit Lab02 - PY102001020
    
    * Submit Lab03 - PY102001020
    
    * Submit Lab04 - PY102001020
    
    * Lab04 submission - PY102001020
    
    * Lab04 submission - PY102001020
    
    * Submit Lab05 - PY102001020
    
    * Submit Lab06 - PY102001020
    
    ---------
    
    Co-authored-by: Dr Myo Thida <88795729+myothida@users.noreply.github.com>

---
+++ submissions/PY102001020/lab06.py
@@ -0,0 +1,139 @@
+# -------------------------
+# Do not change the below Code
+# -------------------------
+def helper_fun1_(arr, i):
+    while i > 0:
+        parent = (i - 1) // 2
+        if arr[i] < arr[parent]:
+            arr[i], arr[parent] = arr[parent], arr[i]
+            i = parent
+        else:
+            break
+
+    return arr
+
+def helper_fun2_(arr, i):
+    n = len(arr)
+
+    while True:
+        left = 2 * i + 1
+        right = 2 * i + 2
+        smallest = i
+        if left < n and arr[left] < arr[smallest]:
+            smallest = left
+        if right < n and arr[right] < arr[smallest]:
+            smallest = right
+        if smallest != i:
+            arr[i], arr[smallest] = arr[smallest], arr[i]
+            i = smallest
+        else:
+            break
+    return arr
+
+# ------------------------------------------------------------
+# Q1 — schedule_next_job
+# ------------------------------------------------------------
+# A cloud system maintains a job list where each job has a
+# priority score (smaller value = higher priority).
+#
+# The system must always be able to quickly access (O(1)) the job
+# with the highest priority (smallest score).
+#
+# You are given the current job list (arr) and a new job
+# (priority_score).
+#
+# Write a function to add a new job into the system
+# and and reorganize the list so that the priority rule is maintained.
+
+# IMPORTANT:
+# - You may use the provided helper functions to adjust the structure.
+# - Do NOT implement any additional helper functions.
+# ------------------------------------------------------------
+
+def schedule_next_job(jobs, new_job):
+    jobs.append(new_job)
+    helper_fun1_(jobs, len(jobs) - 1)
+    return jobs
+
+    # TODO: insert job
+    # TODO: move it upward as needed (like heap behavior)
+    
+
+
+# ------------------------------------------------------------
+# Q2 — process_next_job
+# ------------------------------------------------------------
+# A system processes jobs based on priority score
+# (smaller value = higher priority).
+#
+# The job with the highest priority is always processed first.
+#
+# Given the current job list (arr), remove and return the
+# next job to process.
+#
+# After removal, reorganize the list to maintain the
+# priority rule.
+#
+# IMPORTANT:
+# - You may use the provided helper functions to adjust the structure.
+# - Do NOT implement any additional helper functions.
+#
+# ------------------------------------------------------------
+
+def process_next_job(arr):
+    if len(arr) == 0:
+        return None
+
+    highest_priority_job = arr[0]
+    arr[0] = arr[-1]
+    arr.pop()
+
+    if len(arr) > 0:
+        helper_fun2_(arr,0)
+    return highest_priority_job, arr
+
+    # TODO: return removed job and updated list
+
+    
+
+# ------------------------------------------------------------
+# Q3 — personal priority reflection
+# ------------------------------------------------------------
+# This question is about reflecting on your personal priorities.
+#
+# You are given a structure where each item is:
+#     (weight, category)
+#
+# Smaller weight = higher priority.
+#
+# Instructions:
+# 1. Change the weights (numbers) based on YOUR priorities.
+# 2. Do NOT change the category names.
+# 3. Insert a new category ("security") with your chosen weight.
+# 4. Maintain the structure using the provided helper function.
+#
+# NOTE:
+# - There is NO single correct answer.
+# - Focus on meaningful weights based on your perspective.
+# - Do NOT create additional helper functions.
+# ------------------------------------------------------------
+
+def personal_priority_q():
+    priorty_q = [
+        (3, "education"),
+        (2, "family"),
+        (1, "health"),
+        (5, "friends"),
+        (4, "money")
+    ]
+    # TODO: append new_item to personal_priorty_q
+    
+    new_item = (3, "security")
+    priorty_q.append(new_item)
+    
+    # TODO: return the list 
+    # highest priority (lowest score) should be always top
+
+    helper_fun1_(priorty_q, len(priorty_q) - 1)
+    return priorty_q
+
